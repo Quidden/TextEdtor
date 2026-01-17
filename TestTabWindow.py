f14a1799f00278f5e0d7299f72598b0b3fdf99b3
Quidden <98659143+Quidden@users.noreply.github.com>
2026-01-17 23:28:31 +0200

    Add Qbox in GUI

--- TestTabWindow.py
+++ TestTabWindow.py
@@ -1,4 +1,6 @@
 import PyQt6
+from PyQt6.QtCore import \
+    Qt
 from PyQt6.QtGui import \
     QPixmap
 from PyQt6.QtWidgets import \
@@ -9,7 +11,8 @@ from PyQt6.QtWidgets import \
     QWidget, \
     QLabel, \
     QTabWidget, \
-    QComboBox
+    QComboBox, \
+    QGroupBox
 from PyQt6.uic.properties import \
     QtWidgets
 
@@ -32,26 +35,29 @@ class Program(QWidget):
         self.h_layout.addLayout(self.v_textlayout, 4)
         self.h_layout.addLayout(self.v_settingslayout, 2)
 
-        self.v_enualayout.addWidget(PuctureResult())
-        self.v_enualayout.addWidget(ResultWidget())
-        self.v_enualayout.addWidget(ResultWidget())
+        self.v_enualayout.addWidget(ImageResult())
+        self.TextBoxResult1 = ResultWidget()
+        self.TextBoxResult1.box.setTitle('EN Text')
+        self.TextBoxResult2 = ResultWidget()
+        self.TextBoxResult2.box.setTitle('UKR Text')
+        self.v_enualayout.addWidget(self.TextBoxResult1)
+        self.v_enualayout.addWidget(self.TextBoxResult2)
 
         self.v_textlayout.addWidget(TextWidget())
 
-        self.tabWidget = QTabWidget()
-        self.tabWidget.addTab(QTextEdit(), "Black list")
-        self.tabWidget.addTab(QTextEdit(), "Settings")
-        self.tabWidget.addTab(QTextEdit(), "Log")
-        self.v_settingslayout.addWidget(self.tabWidget)
+        self.v_settingslayout.addWidget(SettingsTab())
         self.v_settingslayout.addWidget(GenButtons())
 
 
 class ResultWidget(QWidget):
     def __init__(self):
         super().__init__()
+        self.text = ''
+        self.box = QGroupBox(self.text)
         self.v_layout = QVBoxLayout()
+        self.box.setLayout(self.v_layout)
+
         self.h_layout = QHBoxLayout()
-        self.setLayout(self.v_layout)
 
         self.enText = QTextEdit()
         self.copyButton = QPushButton("Copy")
@@ -67,28 +73,45 @@ class ResultWidget(QWidget):
         self.h_layout.addWidget(self.techButton)
         self.h_layout.addWidget(self.techButton2)
 
+        self.mainlayout = QVBoxLayout()
+        self.setLayout(self.mainlayout)
+        self.mainlayout.addWidget(self.box)
+
 
 class TextWidget(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.box = QGroupBox("General Text Editor")
         self.v_layout = QVBoxLayout()
-        self.setLayout(self.v_layout)
+        self.box.setLayout(self.v_layout)
 
-        self.Pict = QLabel()
-        self.Pict.setMinimumHeight(200)
-        self.v_layout.addWidget(self.Pict)
+        self.imageBox = QGroupBox("Image DropBox")
+        imageLayout = QVBoxLayout(self.imageBox)
+
+        self.image = QLabel()
+        self.image.setMinimumHeight(150)
+        self.image.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.image.setText('Preview')
+        imageLayout.addWidget(self.image)
+
+        self.v_layout.addWidget(self.imageBox)
 
         self.GeneralText = QTextEdit()
         self.v_layout.addWidget(self.GeneralText)
+
+
+        self.mainlayout = QVBoxLayout()
+        self.setLayout(self.mainlayout)
+        self.mainlayout.addWidget(self.box)
 
 
 
 class GenButtons(QWidget):
     def __init__(self):
         super().__init__()
+        self.box = QGroupBox("General Buttons")
         self.v_layout = QVBoxLayout()
-        self.setLayout(self.v_layout)
+        self.box.setLayout(self.v_layout)
 
         self.button1 = QPushButton("Button1")
         self.button2 = QPushButton("Button2")
@@ -104,24 +127,59 @@ class GenButtons(QWidget):
         self.v_layout.addWidget(self.button5)
         self.v_layout.addWidget(self.button6)
 
+        self.mainlayout = QVBoxLayout()
+        self.setLayout(self.mainlayout)
+        self.mainlayout.addWidget(self.box)
 
-class PuctureResult(QWidget):
+
+class ImageResult(QWidget):
     def __init__(self):
         super().__init__()
-        self.v_layout = QVBoxLayout()
-        self.setLayout(self.v_layout)
 
-        self.pictresult = QLabel()
-        self.pictresult.setMinimumHeight(150)
-        self.v_layout.addWidget(self.pictresult)
+        # Внешняя рамка секции
+        self.box = QGroupBox("Convert")
+        box_layout = QVBoxLayout(self.box)
 
-        self.h_layout = QHBoxLayout()
+        # Внутренняя рамка только для картинки
+        self.boxImage = QGroupBox("Image result")
+        image_layout = QVBoxLayout(self.boxImage)
 
+        self.imageresult = QLabel("preview")
+        self.imageresult.setMinimumHeight(150)
+        self.imageresult.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        image_layout.addWidget(self.imageresult)
+
+        # Добавляем внутренний groupbox в внешний
+        box_layout.addWidget(self.boxImage)
+
+        # Панель управления (формат + кнопка)
+        controls = QHBoxLayout()
         self.combobox = QComboBox()
         self.combobox.addItems(["WebP", "PNG", "JPEG"])
-
         self.resButton = QPushButton("Result")
 
-        self.h_layout.addWidget(self.combobox)
-        self.h_layout.addWidget(self.resButton)
-        self.v_layout.addLayout(self.h_layout)
+        controls.addWidget(self.combobox)
+        controls.addWidget(self.resButton)
+        box_layout.addLayout(controls)
+
+        # Layout для самого виджета ImageResult
+        mainlayout = QVBoxLayout(self)
+        mainlayout.addWidget(self.box)
+
+class SettingsTab(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.box = QGroupBox("Settings")
+        self.v_layout = QVBoxLayout()
+        self.box.setLayout(self.v_layout)
+
+        self.tabWidget = QTabWidget()
+        self.tabWidget.addTab(QTextEdit(), "Black list")
+        self.tabWidget.addTab(QTextEdit(), "Settings")
+        self.tabWidget.addTab(QTextEdit(), "Log")
+        self.v_layout.addWidget(self.tabWidget)
+
+        self.mainlayout = QVBoxLayout()
+        self.setLayout(self.mainlayout)
+        self.mainlayout.addWidget(self.box)
