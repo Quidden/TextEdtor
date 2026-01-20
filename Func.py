823c1ff0ba5f4829875c3b29fad8cf5f14688bf4
Quidden <98659143+Quidden@users.noreply.github.com>
2026-01-20 22:23:37 +0200

    json work

--- Func.py
+++ Func.py
@@ -1,3 +1,37 @@
+import json
+import os
+
 def test(program):
     main_text = program.TextWidget.get_general_text()
     program.TextBoxResult1.set_text(main_text)
+
+def black_list_load(*, black_list: str, white_list: str):
+
+    temp = []
+
+    if not os.path.exists("black_list.json"):
+        with open("black_list.json", "w") as outfile:
+            json.dump([],outfile)
+
+
+    with open("black_list.json", "r") as read_file:
+        temp = json.load(read_file)
+
+    id = len(temp)
+
+    item = {"id": id, "black_list": black_list, "white_list": white_list}
+
+    temp.append(item)
+
+    json_object = json.dumps(temp, indent=4, sort_keys=True, ensure_ascii=False)
+
+    with open("black_list.json", "w") as outfile:
+        outfile.write(json_object)
+
+    for tems in temp:
+        print(tems)
+
+
+
+black_list_load(black_list="123423sfdgsdfgsdfg", white_list="45a34242sd6")
+
