c62fc033d0200ae4855802673510f6fe87735d4a
Quidden <98659143+Quidden@users.noreply.github.com>
2026-01-25 12:23:19 +0200

    Refactored `TestTabWindow` to introduce `BlackListMenu` class, improve layout structure, and enhance clarity in black list handling

--- TestTabWindow.py
+++ TestTabWindow.py
@@ -52,7 +52,7 @@ class Program(QWidget):
         self.v_settings_layout = QVBoxLayout()
         self.SettingsTab = SettingsTab()
         self.v_settings_layout.addWidget(self.SettingsTab)
-        self.SettingsTab.confirm_button.clicked.connect(lambda : self.bl_list_func())
+        self.SettingsTab.black_list_menu.confirm_button.clicked.connect(lambda : self.bl_list_func())
         self.GenButtons = GenButtons()
         self.v_settings_layout.addWidget(self.GenButtons)
         self.but = self.GenButtons.buttons()
@@ -63,8 +63,8 @@ class Program(QWidget):
     def bl_list_func(self):
 
         if not black_list_load(
-                black_list=self.SettingsTab.black_list_item.text(),
-                white_list=self.SettingsTab.white_list_item.text()):
+                black_list=self.SettingsTab.black_list_menu.black_list_item.text(),
+                white_list=self.SettingsTab.black_list_menu.white_list_item.text()):
             self.msg_box = QMessageBox()
             self.setWindowTitle("Error")
             self.msg_box.setText("Replace")
@@ -75,11 +75,11 @@ class Program(QWidget):
                 return
 
         black_list_load(
-            black_list=self.SettingsTab.black_list_item.text(),
-            white_list=self.SettingsTab.white_list_item.text())
-        self.SettingsTab.clear_black_list()
+            black_list=self.SettingsTab.black_list_menu.black_list_item.text(),
+            white_list=self.SettingsTab.black_list_menu.white_list_item.text())
+        self.SettingsTab.black_list_menu.clear_black_list()
         for item in refresh_black_list():
-            self.SettingsTab.add_black_list_item(str(item))
+            self.SettingsTab.black_list_menu.add_black_list_item(str(item))
 
     def replace_text(self):
         self.TextWidget.set_general_text(Func.accept_black_list(
@@ -255,98 +255,31 @@ class SettingsTab(QWidget):
         self.v_layout = QVBoxLayout()
         self.box.setLayout(self.v_layout)
 
-        self.tabWidget = QTabWidget()
-        self.b_list = QListWidget()
+        self.tab_widget = QTabWidget()
 
-        for item in refresh_black_list():
-            self.b_list.addItem(str(item))
-        self.b_list.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        self.b_list.customContextMenuRequested.connect(self.open_menu)
-
-        self.refresh_button = QPushButton("Refresh")
-        self.delete_item = QPushButton("Delete")
-        self.refresh_button.setVisible(False)
-        self.delete_item.setVisible(False)
-        self.h_button_layout = QHBoxLayout()
-        self.h_button_layout.addWidget(self.refresh_button)
-        self.h_button_layout.addWidget(self.delete_item)
-
-        self.v2_widget = QWidget()
-        self.v2_layout = QVBoxLayout(self.v2_widget)
-        self.v2_layout.addWidget(self.b_list)
-        self.v2_layout.addLayout(self.h_button_layout)
-
-        self.second_box = QGroupBox("Black list add item tab")
-        self.v3_layout = QVBoxLayout()
-        #self.v2_layout.addLayout(self.v3_layout)
-        self.h2_layout = QHBoxLayout()
-        self.black_list_item = QLineEdit()
-        self.black_list_item.setPlaceholderText("Blacklist Item")
-        self.white_list_item = QLineEdit()
-        self.white_list_item.setPlaceholderText("Whitelist Item")
-
-        self.h2_layout.addWidget(self.black_list_item)
-        self.h2_layout.addWidget(QLabel("->"))
-        self.h2_layout.addWidget(self.white_list_item)
-        self.v3_layout.addLayout(self.h2_layout)
-
-        self.confirm_button = QPushButton("Confirm")
-        self.v3_layout.addWidget(self.confirm_button)
-
-        self.second_box.setLayout(self.v3_layout)
-        self.v2_layout.addWidget(self.second_box)
-
-        self.tabWidget.addTab(self.v2_widget, "Black list")
+        self.black_list_menu = self.BlackListMenu()
+        self.tab_widget.addTab(self.black_list_menu, "Black list")
         self.setting_menu = self.SettingMenu()
-        self.tabWidget.addTab(self.setting_menu, "Settings")
+        self.tab_widget.addTab(self.setting_menu, "Settings")
         self.log = QTextEdit()
-        self.tabWidget.addTab(self.log, "Log")
-        self.v_layout.addWidget(self.tabWidget)
+        self.tab_widget.addTab(self.log, "Log")
+        self.v_layout.addWidget(self.tab_widget)
 
-        self.mainlayout = QVBoxLayout()
-        self.setLayout(self.mainlayout)
-        self.mainlayout.addWidget(self.box)
-
-    def open_menu(self, position):
-        item = self.b_list.itemAt(position)
-
-        menu = QMenu(self)
-
-        delete = menu.addAction("Delete")
-        action = menu.exec(self.b_list.mapToGlobal(position))
-        if action == delete:
-            black_list_item_delete(self.b_list.row(item))
-            self.clear_black_list()
-            for item in refresh_black_list():
-                self.b_list.addItem(str(item))
+        self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
+        self.main_layout.addWidget(self.box)
 
 
     def get_tab_widget(self):
-        return self.tabWidget
+        return self.tab_widget
     def get_box(self):
         return self.box
-
-    def get_black_list(self):
-        return self.b_list
-    def add_black_list_item(self, text):
-        self.b_list.addItem(text)
-    def clear_black_list(self):
-        self.b_list.clear()
 
     def get_log(self):
         return self.log
     def set_log(self, text):
         self.log.setPlainText(text)
 
-    def get_refresh_button(self):
-        return self.refresh_button
-
-    def get_black_list_item(self):
-        return self.black_list_item
-    def get_white_list_item(self):
-        return self.white_list_item
-    def get_confirm_button(self):
-        return self.confirm_button
 
     class SettingMenu(QWidget):
         def __init__(self):
@@ -371,3 +304,79 @@ class SettingsTab(QWidget):
             return self.check_box_text_settings.isChecked()
         def get_check_box_text_empty_settings(self):
             return self.check_box_text_empty_settings.isChecked()
+
+    class BlackListMenu(QWidget):
+        def __init__(self):
+            super().__init__()
+
+            self.b_list = QListWidget()
+
+            for item in refresh_black_list():
+                self.b_list.addItem(str(item))
+            self.b_list.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+            self.b_list.customContextMenuRequested.connect(self.open_menu)
+
+            self.refresh_button = QPushButton("Refresh")
+            self.delete_item = QPushButton("Delete")
+            self.refresh_button.setVisible(False)
+            self.delete_item.setVisible(False)
+            self.h_button_layout = QHBoxLayout()
+            self.h_button_layout.addWidget(self.refresh_button)
+            self.h_button_layout.addWidget(self.delete_item)
+
+            self.v2_widget = QWidget()
+            self.v2_layout = QVBoxLayout(self.v2_widget)
+            self.v2_layout.addWidget(self.b_list)
+            self.v2_layout.addLayout(self.h_button_layout)
+
+            self.second_box = QGroupBox("Black list add item tab")
+            self.v3_layout = QVBoxLayout()
+            self.h2_layout = QHBoxLayout()
+            self.black_list_item = QLineEdit()
+            self.black_list_item.setPlaceholderText("Blacklist Item")
+            self.white_list_item = QLineEdit()
+            self.white_list_item.setPlaceholderText("Whitelist Item")
+
+            self.h2_layout.addWidget(self.black_list_item)
+            self.h2_layout.addWidget(QLabel("->"))
+            self.h2_layout.addWidget(self.white_list_item)
+            self.v3_layout.addLayout(self.h2_layout)
+
+            self.confirm_button = QPushButton("Confirm")
+            self.v3_layout.addWidget(self.confirm_button)
+
+            self.second_box.setLayout(self.v3_layout)
+            self.v2_layout.addWidget(self.second_box)
+
+            self.setLayout(self.v2_layout)
+
+
+        def open_menu(self, position):
+            item = self.b_list.itemAt(position)
+
+            menu = QMenu(self)
+
+            delete = menu.addAction("Delete")
+            action = menu.exec(self.b_list.mapToGlobal(position))
+            if action == delete:
+                black_list_item_delete(self.b_list.row(item))
+                self.clear_black_list()
+                for item in refresh_black_list():
+                    self.b_list.addItem(str(item))
+
+        def get_black_list(self):
+            return self.b_list
+        def add_black_list_item(self, text):
+            self.b_list.addItem(text)
+        def clear_black_list(self):
+            self.b_list.clear()
+
+        def get_black_list_item(self):
+            return self.black_list_item
+        def get_white_list_item(self):
+            return self.white_list_item
+        def get_confirm_button(self):
+            return self.confirm_button
+
+        def get_refresh_button(self):
+            return self.refresh_button
